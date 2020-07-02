723743d4052ffbe809a377a7a870aadb99722180
vikrant <vikrantthakur15@gmail.com>
2020-07-02 17:51:21 +0530

    changes added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
+var productMap = {};
+function mainList(){
+	var map = {};
+	map["main-1"]="All Products";
+	map["main-2"]="Patravali";
+	map["main-3"]="Paper Dish";
+	map["main-4"]="Paper Dona";
+	map["main-5"]="Spoon";
+	map["main-6"]="Tissue Paper";
+	map["main-7"]="Paper Glass";		
+	createMainList(map);
+}
 
+function productList(){	
+	productMap["product-1"]="Silver-White Flat Thali#main-2#This dish was design by silver foil paper from both side It is made in circular shape with small design.<br><br><b>Quantity: 25 piece per packet </b><br><br><b>Price: Rs 70 per packet. </b>";
+	productMap["product-2"]="Brown Leaf Dish#main-2#It is mainly made from Sal leaves and Banyan tree leaves. It is made in circular shape and compartment inside, by stitching 6 to 8 sal leaves with tiny wooden sticks.<br><br><b>Quantity: 25 piece per packet </b><br><br><b>Price: Rs 40 per packet. </b>";
+	productMap["product-3"]="Silver Both Side Thali#main-2#It is mainly made from Silver foil paper from both side. It is made in circular shape and compartment inside.<br><br><b>Quantity: 25 piece per packet </b><br><br><b>Price: Rs 60 per packet. </b>";
+	productMap["product-4"]="Silver-White Brown Ploy#main-2#It is mainly made from Silver/White foil paper and Brown paper. It is made in circular shape and compartment inside.<br><br><b>Quantity: 25 piece per packet </b><br><br><b>Price: Rs 65 per packet. </b>";
+	productMap["product-5"]="Silver-White White Poly#main-2#It is mainly made from Silver/White foil paper and White paper. It is made in circular shape and compartment inside.<br><br><b>Quantity: 25 piece per packet </b><br><br><b>Price: Rs 70 per packet. </b>";
+	productMap["product-6"]="Silver Dish 4-inch#main-3#This dish was design by silver foil paper from both side.<br><br><b>Quantity: 30 piece per packet </b><br><br><b>Size: 4-inch</b><br><br><b>Price: Rs 20 per packet. </b>";
+	productMap["product-7"]="Silver Dish 5-inch#main-3#This dish was design by silver foil paper from both side.<br><br><b>Quantity: 30 piece per packet </b><br><br><b>Size: 5-inch</b><br><br><b>Price: Rs 30 per packet. </b>";
+	productMap["product-8"]="White Dish 4-inch#main-3#This dish was design by white paper from both side.<br><br><b>Quantity: 30 piece per packet </b><br><br><b>Size: 4-inch</b><br><br><b>Price: Rs 15 per packet. </b>";
+	productMap["product-9"]="White Dish 5-inch#main-3#This dish was design by white paper from both side.<br><br><b>Quantity: 30 piece per packet </b><br><br><b>Size: 5-inch</b><br><br><b>Price: Rs 25 per packet. </b>";
+	productMap["product-10"]="Silver Dona#main-4#This Dona was design by silver paper from both side.<br><br><b>Quantity: 30 piece per packet </b><br><br><b>Price: Rs 12 per packet. </b>";
+	productMap["product-11"]="Plastic White Spoon#main-5#This small is white in color made with plastic.<br><br><b>Quantity: 100 piece per packet </b><br><br><b>Price: Rs 12 per packet. </b>";
+	productMap["product-12"]="Tissue Paper#main-6#This soft tissue paper used for handwash, They are white in color.<br><br><b>Quantity: 100 piece per packet </b><br><br><b>Price: Rs 13 per packet. </b>";
+	productMap["product-13"]="Cold Drink Glass#main-7#This glass is used for Cold Drink, Made up of white paper with brown design from all corners, very handy to use.<br><br><b>Quantity: 50 piece per packet </b><br><br><b>Size: 120 ml of capacity. </b><br><br><b>Price: Rs 40 per packet. </b>";
+	productMap["product-14"]="IceCream Cup#main-7#This glass is used for Ice-cream, Made up of white paper, very handy to use.<br><br><b>Quantity: 50 piece per packet </b><br><br><b>Size: 120 ml of capacity. </b><br><br><b>Price: Rs 45 per packet. </b>";
+	productMap["product-15"]="Tea Glass#main-7#This glass is used for Tea Drink, Made up of white paper, very handy to use.<br><br><b>Quantity: 50 piece per packet </b><br><br><b>Size: 90 ml of capacity. </b><br><br><b>Price: Rs 25 per packet. </b>";
+	productMap["product-16"]="Water Glass#main-7#This glass is used for Water Drink, Made up of white paper with brown design from all corners, very handy to use.<br><br><b>Quantity: 50 piece per packet </b><br><br><b>Size: 160 ml of capacity. </b><br><br><b>Price: Rs 60 per packet. </b>";	
+	createProductList(productMap);	
+}
 
 	$(document).ready(function () {
-		
+		  mainList();
 		$(".filter").click(function (){
 			
 			$(".filter").removeClass("active");
@@ -10,15 +41,11 @@
 			$(this).addClass("active");
 			$("."+$(this).attr('data-id')).show();
 			
-			if("all" == $(this).attr('data-id')){
+			if("main-1" == $(this).attr('data-id')){
 				$(".item").show();
 			}
 		})
 		
-		$('#portfolio').mixitup({
-                targetSelector: '.item',
-                transitionSpeed: 350
-            });
 		
 		$(document).on("scroll", onScroll);
  
@@ -40,6 +67,8 @@
 				$(document).on("scroll", onScroll);
 			});
 		});
+		
+		checkCartStatus();
 	});
  
 	function onScroll(event){
@@ -67,11 +96,113 @@
             
         });*/
    };
+  
+
+function createMainList(map){
+	var str="";
+	$.each(map, function(key,val){
+       str = str + '<li class="filter" data-id="'+key+'">'+val+'</li>';
+    });
+	$(".mainListClass").html(str);
+	$(".mainListClass > li:first").addClass("active");
+	productList();
+}	
 
 
-function generateProductList(){
-	var str = '[{"id":"27-06-2020-10:50:37","productId":1,"productName":"Flat Thali (25 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"70","maxPrice":"70","discount":" ","stockStatus":"Y","productDesc":"Flat Silver - White"}]},{"id":"27-06-2020-10:54:34","productId":2,"productName":"Leaf Thali (25 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"40","maxPrice":"40","discount":" ","stockStatus":"Y","productDesc":"Brown Leaf Thali"}]},{"id":"27-06-2020-10:57:22","productId":3,"productName":"Silver Thali (25 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"60","maxPrice":"60","discount":" ","stockStatus":"Y","productDesc":"Both Side Silver"},{"id":1,"minPrice":"65","maxPrice":"65","discount":" ","stockStatus":"Y","productDesc":"Silver and Brown Poly"},{"id":2,"minPrice":"70","maxPrice":"70","discount":" ","stockStatus":"Y","productDesc":"Silver and White Poly"}]},{"id":"27-06-2020-10:59:35","productId":4,"productName":"Silver Dish (30 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"20","maxPrice":"20","discount":" ","stockStatus":"Y","productDesc":"4 inch"},{"id":1,"minPrice":"30","maxPrice":"30","discount":" ","stockStatus":"Y","productDesc":"5 inch"}]},{"id":"27-06-2020-11:00:21","productId":5,"productName":"White Dish (30 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"15","maxPrice":"15","discount":" ","stockStatus":"Y","productDesc":"4 inch"},{"id":1,"minPrice":"25","maxPrice":"25","discount":" ","stockStatus":"Y","productDesc":"5 inch"}]},{"id":"27-06-2020-11:01:53","productId":6,"productName":"Silver Dona (30 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"12","maxPrice":"12","discount":" ","stockStatus":"Y","productDesc":"Silver Dona"}]},{"id":"28-06-2020-11:14:30","productId":7,"productName":"Cold Drink Cup (50 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"40","maxPrice":"40","discount":" ","stockStatus":"Y","productDesc":"120ml"}]},{"id":"28-06-2020-11:15:14","productId":8,"productName":"Ice-Cream Cup (50 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"45","maxPrice":"45","discount":" ","stockStatus":"Y","productDesc":"120ml"}]},{"id":"28-06-2020-11:15:50","productId":9,"productName":"Tea Glass  (50 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"25","maxPrice":"25","discount":" ","stockStatus":"Y","productDesc":"90ml"}]},{"id":"28-06-2020-11:16:24","productId":10,"productName":"Water Glass  (50 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"60","maxPrice":"60","discount":" ","stockStatus":"Y","productDesc":"160ml"}]},{"id":"28-06-2020-11:17:10","productId":11,"productName":"Spoon (100 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"25","maxPrice":"25","discount":" ","stockStatus":"Y","productDesc":"Spoon"}]},{"id":"28-06-2020-11:18:07","productId":12,"productName":"Tissue Paper (100 piece)","stockStatus":"Y","category":"P","productDetailsList":[{"id":0,"minPrice":"15","maxPrice":"15","discount":" ","stockStatus":"Y","productDesc":"Tissue Paper"}]}]';
-	$(JSON.parse(str)).each(function(){
-		console.log(this);
-	})	
+
+function createProductList(map){
+	var str="";
+	$.each(map, function(key,val){
+		var value = val.split("#");
+       str = str + '<li class="item '+value[1]+'"><img src="images/'+key+'.jpg" alt="Food" ><h2 class="">'+value[0]+'</h2><div class="row" style="margin-bottom:10px"><div class=""><input type="button" value="Add to Order" class="btn btn-primary btn-sm" data-id='+key+' onClick="return addToCart(this)"/><input type="button" style="margin-left:5px" value="View Details" class="btn btn-primary btn-sm" data-id='+key+' onClick="return displayModalDetails(this)" /></div></div></li>';
+    });
+	$("#portfolio").html(str);	
 }
+
+function addToCart(obj){
+	$("#modalLabel").html("Add to Cart Details");
+	$("#modal-body-content").html(productMap[$(obj).attr('data-id')].split("#")[0] + " Successfully added to Order section.");
+	$("#detailsModal").modal('show');		
+	$("#cartTable").append('<tr><td>'+(++$("#cartTable > tr").length)+'</td><td><img src="images/'+$(obj).attr('data-id')+'.jpg" alt="Food" style="height:80px"></td><td>'+productMap[$(obj).attr('data-id')].split("#")[0]+'</td><td><input type="number" size="2" class="qty" style="width:55px" value="1"></td><td><input type="button" onClick="return removeCart(this)" value="Remove"></td></tr>');
+	checkCartStatus();
+}
+
+function displayModalDetails(obj){	
+	$("#modalLabel").html(productMap[$(obj).attr('data-id')].split("#")[0] + " Details");
+	var str = '<div class="row"><div class="col-md-5" id="modal-img"><img src="images/'+$(obj).attr('data-id')+'.jpg" height="200px" width="200px" /></div><div class="col-md-7" id="modal-content">'+productMap[$(obj).attr('data-id')].split("#")[2]+'</div></div>';
+	$("#modal-body-content").html(str);
+	$("#detailsModal").modal('show');	
+}
+
+function removeCart(obj){
+	
+	$(obj).parent().parent().remove();
+	checkCartStatus();
+}
+
+function checkCartStatus(){
+	if($("#cartTable > tr").length == 0){
+		$("#cartImgDiv").show();
+		$("#cartTableDiv").hide();
+	}else{
+		$("#cartImgDiv").hide();
+		$("#cartTableDiv").show();
+	}
+	
+}
+function closePopup(){	
+	$("#detailsModal").modal('hide');	
+}
+
+function submitOrder(obj){
+	if($("#cartTable > tr").length == 0){
+		alert("Please add some Item in cart befor placing Order.");
+		return false;
+	}
+    
+    if($("#name").val() == '' ){
+		   alert('Please enter your Name.');
+		   $("#name").focus()
+		   return false;
+	   	}
+		 if($("#email").val() == '' ){
+		   alert('Please enter a valid email.');
+		 $("#email").focus()
+		   return false;
+	   	}
+		 if($("#Phone").val() == '' ){
+		   alert('Please enter 10 digit Phone/Mobile Number.');
+		   $("#Phone").focus()
+		   return false;
+		 }
+
+		  $(obj).attr('disabled',true);
+		  $(obj).attr('value','Please wait ...');
+		 var orderMgs = "<br><br><br><b>Below are list of Products Ordered </b><br><ol>";
+		$("#cartTable > tr").each(function(){
+			orderMgs = orderMgs + "<li>" + $(this).find("td").eq(2).html() +", With Quantity "+ $(this).find("td").eq(3).find('.qty').val()+"</li>";
+		});
+		orderMgs = orderMgs + "</ol>";
+		
+		  var map={}
+		  map["name"]=$("#name").val();
+		  map["emailid"]=$("#email").val();
+		  map["phoneNo"]=$("#Phone").val();
+		  map["message"]=$("#message").val();
+		  map["otherDetails"]=orderMgs;
+		  $.ajax({
+		   type: 'POST',
+		   data: JSON.stringify(map),
+		   url: "https://5vvnjl5hii.execute-api.ap-south-1.amazonaws.com/dev/MP/sendEmail",
+		   success: function (response) {
+		  alert("Thanks you, for Submitting order, We will get back you soon.");
+		  location.reload();
+		  },
+		   error : function (response) {
+		  alert("Error while submitting response, Please contact admin")
+		  location.reload();
+		  }
+		  
+		  });
+  return false;
+  }
